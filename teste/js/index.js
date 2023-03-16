98575c25253bc671fec05efd618bb16731102c84
mateusrodrigues0904 <125597047+mateusrodrigues0904@users.noreply.github.com>
2023-03-16 15:33:55 -0300

    finalizei o exercicio 2

--- teste/js/index.js
+++ teste/js/index.js
@@ -2,19 +2,40 @@ function verificar() {
   var i, nascimento, idade, sexo, msg, img;
   msg = document.getElementById("imsg");
   img = document.getElementById("iimg");
-  nascimento = document.getElementById("inas");
-  nascimento = nascimento.value;
-  idade = 2023 - nascimento;
   sexo = document.getElementsByName("sexo");
+  nascimento = document.getElementById("inas").value;
+  idade = 2023 - nascimento;
+
   for (i = 0; i < sexo.length; i++) {
     if (sexo[i].checked) {
       sexo = sexo[i].value;
     }
   }
+
+  img.style.opacity = "100%";
+
   if (sexo == "M") {
     msg.innerHTML = `Detectamos um Homem com ${idade} anos.`;
-    if (idade <= 10) {
-      img.style.backgroundImage = "url('../img/mcrianca.jpg')";
+    if (idade > 0 && idade <= 17) {
+      img.style.backgroundImage = "url(img/hcrianca.jpg)";
+    } else if (idade > 17 && idade <= 45) {
+      img.style.backgroundImage = "url(img/hjovem.jpg)";
+      img.style.backgroundPosition = "center";
+    } else if (idade > 45 && idade <= 123) {
+      img.style.backgroundImage = "url(img/hidoso.jpg)";
+    } else {
+      msg.innerHTML = `Digite um valor válido.`;
+    }
+  } else {
+    msg.innerHTML = `Detectamos uma Mulher com ${idade} anos.`;
+    if (idade > 0 && idade <= 17) {
+      img.style.backgroundImage = "url(img/fcrianca.jpg)";
+    } else if (idade > 17 && idade <= 45) {
+      img.style.backgroundImage = "url(img/fjovem.jpg)";
+    } else if (idade > 45 && idade <= 123) {
+      img.style.backgroundImage = "url(img/fidoso.jpg)";
+    } else {
+      msg.innerHTML = `Digite um valor válido.`;
     }
   }
 }
